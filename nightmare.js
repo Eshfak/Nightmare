6b67ef9d3d8fb2b465ce95e32bdd587789f33749
Eshfak <mdeshfakulislam2@gmail.com>
2021-10-10 14:09:52 +0600

    first commit

---
+++ nightmare.js
@@ -0,0 +1,5 @@
+var menubutton= document.querySelector(".button");
+menubutton.addEventListener("click",function () {
+        var slider = document.querySelector(".links");
+        slider.classList.toggle("mobilemenu");
+    }) 
